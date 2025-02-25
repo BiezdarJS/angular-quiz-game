0c392b155059d8adbf75c3a698cda734eeda1659
BiezdarJS <piotrdeja.pl@gmail.com>
2025-02-25 08:46:22 +0100

    Add improvements to translation

--- src/app/2-main/welcome-to-quiz/welcome-to-quiz.component.ts
+++ src/app/2-main/welcome-to-quiz/welcome-to-quiz.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { QuizStoreService } from '../quiz/services/quiz-store.service';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-welcome-to-quiz',
-  imports: [],
+  imports: [TranslocoModule],
   templateUrl: './welcome-to-quiz.component.html',
   styleUrl: './welcome-to-quiz.component.scss'
 })
